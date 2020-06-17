e040bac8d5b33f5a12f181ca8b8566ee55dcf06b
Michael Ford <mford@sfu.ca>
2020-06-17 14:13:28 -0400

    Jupyter first_imports added

---
+++ data-science/first_imports.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+%load_ext autoreload
+%autoreload 2
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+import os
+os.chdir('../')
+
+import random
+rand_seed = 2675
+random.seed(rand_seed)

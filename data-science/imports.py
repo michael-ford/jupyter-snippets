2d2fb06fc9d116c5bd865eb5b80bc4bb612dc391
Michael Ford <mford@sfu.ca>
2020-09-16 13:54:23 -0400

    Add jupyter markdown in code cell hack

--- data-science/imports.py
+++ data-science/imports.py
@@ -1,7 +1,9 @@
 # Import packages
 import os
 from src import common
+from src.data import dvc_tools
 import logbook
+from IPython.display import Markdown as md       # Allows markdown output in code cells. See https://stackoverflow.com/a/57023238
 
 # Setup log
 common.initialize_logger(log_file_path='', overwrite=True)
